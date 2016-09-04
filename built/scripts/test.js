59243cea33b80545962653c1091dbce76dca6e16
Kevin Menhinick <kamenhinick@gmail.com>
2016-09-04 23:59:04 +1200

    Added header title with animation

--- built/scripts/test.js
+++ built/scripts/test.js
@@ -1,3 +1,18 @@
 /// <reference path="lib/jquery.d.ts" />
 var test = 'hello world';
 console.log(test);
+var headerTitle = $('header');
+$(window).on('scroll', function () {
+    var y = window.pageYOffset;
+    var threshold = headerTitle.position().top;
+    var cutoff = headerTitle.height() + threshold + 50;
+    if (y <= threshold) {
+        headerTitle.css('opacity', 1);
+    }
+    else if (y <= cutoff) {
+        headerTitle.css('opacity', (cutoff - (y - threshold)) / cutoff);
+    }
+    else {
+        headerTitle.css('opacity', 0);
+    }
+});
--- src/scripts/test.ts
+++ src/scripts/test.ts
@@ -1,3 +1,18 @@
 /// <reference path="lib/jquery.d.ts" />
 var test: string = 'hello world';
 console.log(test);
+
+var headerTitle = $('header');
+
+$(window).on('scroll', function() {
+    var y: number = window.pageYOffset;
+    var threshold: number = headerTitle.position().top;
+    var cutoff: number = headerTitle.height() + threshold + 50;
+    if(y <= threshold) {
+        headerTitle.css('opacity', 1);
+    } else if(y <= cutoff) {
+        headerTitle.css('opacity', (cutoff - (y - threshold)) / cutoff);
+    } else {
+        headerTitle.css('opacity', 0);
+    }
+});
